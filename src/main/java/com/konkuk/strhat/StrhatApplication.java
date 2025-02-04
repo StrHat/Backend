7298f1ab595dd44cb38851e9243db8879f30eaa3
Joy <munchinthepool64@gmail.com>
2025-02-04 22:31:05 +0900

    Initial Commit

---
+++ src/main/java/com/konkuk/strhat/StrhatApplication.java
@@ -0,0 +1,13 @@
+package com.konkuk.strhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrhatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konkuk/strhat/StrhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konkuk.strhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
