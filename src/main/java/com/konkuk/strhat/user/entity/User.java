ece3ce4c664d726fdf298f88de5c4794ebb5575d
Joy <munchinthepool64@gmail.com>
2025-02-23 00:09:33 +0900

    style: 회원 테이블명 변경

--- src/main/java/com/konkuk/strhat/user/entity/User.java
+++ src/main/java/com/konkuk/strhat/user/entity/User.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseCreatedEntity {
