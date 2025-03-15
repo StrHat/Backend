c62b2c1f14e5dd4e47129a89c819f1e8c103649f
You-Hyuk <cxz6411@naver.com>
2025-03-15 19:03:50 +0900

    feat: JwtProvider 구현

--- src/main/java/com/konkuk/strhat/domain/user/dao/RefreshTokenRepository.java
+++ src/main/java/com/konkuk/strhat/domain/user/dao/RefreshTokenRepository.java
@@ -2,7 +2,9 @@ package com.konkuk.strhat.domain.user.dao;
 
 import com.konkuk.strhat.domain.user.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
+    Optional<RefreshToken> findByEmail(String email);
 }
