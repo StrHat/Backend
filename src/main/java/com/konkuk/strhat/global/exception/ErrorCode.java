49aeb053cf067e6b82082a4a3a17a07fb7cf44cf
You-Hyuk <cxz6411@naver.com>
2025-03-17 13:41:05 +0900

    feat: NotFoundUserException 예외처리

--- src/main/java/com/konkuk/strhat/global/exception/ErrorCode.java
+++ src/main/java/com/konkuk/strhat/global/exception/ErrorCode.java
@@ -8,7 +8,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G500", "서버 내부에 문제가 발생했습니다.");
+    // GLOBAL
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G500", "서버 내부에 문제가 발생했습니다."),
+
+    // USER
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "U404", "유저가 존재하지 않습니다");
+
 
     private final HttpStatus httpStatus;
     private final String code;
