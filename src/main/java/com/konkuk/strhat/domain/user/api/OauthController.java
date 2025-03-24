38910a7e778a167e2939bd88f99a6978c2ecd2a6
You-Hyuk <cxz6411@naver.com>
2025-03-24 13:39:43 +0900

    docs: Swagger API 설명 추가

--- src/main/java/com/konkuk/strhat/domain/user/api/OauthController.java
+++ src/main/java/com/konkuk/strhat/domain/user/api/OauthController.java
@@ -4,6 +4,7 @@ import com.konkuk.strhat.domain.user.application.KakaoOAuthService;
 import com.konkuk.strhat.domain.user.dto.PostKakaoSignInRequest;
 import com.konkuk.strhat.domain.user.dto.PostKakaoSignInResponse;
 import com.konkuk.strhat.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ public class OauthController {
 
     private final KakaoOAuthService kakaoOAuthService;
 
+    @Operation(summary = "카카오 검증 및 로그인", description = "카카오 계정 유무를 검증하고 로그인한다.")
     @PostMapping("/kakao")
     public ApiResponse<PostKakaoSignInResponse> kakaoSignIn(@RequestBody PostKakaoSignInRequest request) {
         PostKakaoSignInResponse response = kakaoOAuthService.getUserProfileByToken(request);
--- src/main/java/com/konkuk/strhat/domain/user/api/UserController.java
+++ src/main/java/com/konkuk/strhat/domain/user/api/UserController.java
@@ -11,6 +11,7 @@ import com.konkuk.strhat.domain.user.dto.PostSignUpRequest;
 import com.konkuk.strhat.domain.user.dto.TokenDto;
 import com.konkuk.strhat.domain.user.entity.CustomUserDetails;
 import com.konkuk.strhat.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,48 +28,56 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입", description = "회원가입한다.")
     @PostMapping("/sign-up")
     public ApiResponse<TokenDto> signIn(@RequestBody PostSignUpRequest request) {
         TokenDto response = userService.createUser(request);
         return ApiResponse.success(response);
     }
 
+    @Operation(summary = "로그아웃" ,description = "로그아웃한다.")
     @PostMapping("/sign-out")
     public ApiResponse<Void> signOut(@AuthenticationPrincipal CustomUserDetails userDetails) {
         userService.processSignOut(userDetails.getEmail());
         return ApiResponse.successOnly();
     }
 
+    @Operation(summary = "유저 정보 조회", description = "유저의 정보를 조회한다.")
     @GetMapping("")
     public ApiResponse<GetUserInfoResponse> readUserInfo(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         GetUserInfoResponse response = userService.findUserInfo(customUserDetails.getId());
         return ApiResponse.success(response);
     }
 
+    @Operation(summary = "기본 정보 변경", description = "유저의 닉네임, 태어난 년도, 성별, 직업을 변경한다.")
     @PatchMapping("/info")
     public ApiResponse<Void> updateUserBasicInfo(@RequestBody PatchBasicInfoRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
         userService.modifyUserBasicInfo(request, userDetails.getId());
         return ApiResponse.successOnly();
     }
 
+    @Operation(summary = "취미 및 힐링 방법 변경", description = "유저의 취미 및 힐링 방법을 변경한다.")
     @PatchMapping("/hobby-healing-style")
     public ApiResponse<Void> updateHobbyHealingStyle(@RequestBody PatchHobbyHealingStyleRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
         userService.modifyHobbyHealingStyle(request, userDetails.getId());
         return ApiResponse.successOnly();
     }
 
+    @Operation(summary = "스트레스 해소 방법 변경", description = "유저의 기존 스트레스 해소 방법을 변경한다.")
     @PatchMapping("/stress-relief-style")
     public ApiResponse<Void> updateStressReliefStyle(@RequestBody PatchStressReliefStyleRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
         userService.modifyStressReliefStyle(request, userDetails.getId());
         return ApiResponse.successOnly();
     }
 
+    @Operation(summary = "성향 변경", description = "유저의 기존 성향 정보를 변경한다.")
     @PatchMapping("/personality")
     public ApiResponse<Void> updatePersonality(@RequestBody PatchPersonalityRequest request, @AuthenticationPrincipal CustomUserDetails userDetails) {
         userService.modifyPersonality(request, userDetails.getId());
         return ApiResponse.successOnly();
     }
 
+    @Operation(summary = "토큰 재발급", description = "AccessToken을 재발급한다.")
     @PostMapping("/reissue-token")
     public ApiResponse<Void> reissueToken(@RequestBody PostReissueTokenRequest request) {
         userService.processReissue(request);
