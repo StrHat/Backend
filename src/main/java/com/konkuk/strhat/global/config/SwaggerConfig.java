16e932128f5d841d31f7e7688ddd2c8ceaaea992
You-Hyuk <cxz6411@naver.com>
2025-03-26 22:34:04 +0900

    docs: Swagger 서버 URL 설정

--- src/main/java/com/konkuk/strhat/global/config/SwaggerConfig.java
+++ src/main/java/com/konkuk/strhat/global/config/SwaggerConfig.java
@@ -3,15 +3,34 @@ package com.konkuk.strhat.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server.url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
+        Server server = new Server();
+        server.setUrl(serverUrl);
+
         return new OpenAPI()
-                .components(new Components())
+                .servers(List.of(server))
+                .components(new Components().addSecuritySchemes("AccessToken", new SecurityScheme()
+                        .name("Authorization")
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                ))
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("AccessToken"))
                 .info(apiInfo());
     }
 
