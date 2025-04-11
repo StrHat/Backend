81f82ded6d5044471d5c6647766c4be343e152bb
You-Hyuk <cxz6411@naver.com>
2025-04-11 18:25:50 +0900

    feat: 어노테이션 사용 및 index 추가

--- src/main/java/com/konkuk/strhat/domain/selfDiagnosis/enums/PHQ9Question.java
+++ src/main/java/com/konkuk/strhat/domain/selfDiagnosis/enums/PHQ9Question.java
@@ -1,41 +1,50 @@
 package com.konkuk.strhat.domain.selfDiagnosis.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+
+@Getter
+@RequiredArgsConstructor
 public enum PHQ9Question {
     QUESTION_1(
+            1,
             "기분이 가라앉거나, 우울하거나, 희망이 없다고 느꼈다."
     ),
     QUESTION_2(
+            2,
             "평소 하던 일에 흥미가 없어지거나 즐거움을 느끼지 못했다."
     ),
     QUESTION_3(
+            3,
             "잠들기가 어렵거나 자꾸 깼다. / 혹은 너무 많이 잤다."
     ),
     QUESTION_4(
+            4,
             "평소보다 식욕이 줄었다. / 혹은 평소보다 많이 먹었다."
     ),
     QUESTION_5(
+            5,
             "다른 사람들이 눈치 챌 정도로 평소보다 말과 행동이 느려졌다. / 혹은 너무 안절부절 못해서 가만히 앉아있을 수 없었다."
     ),
     QUESTION_6(
+            6,
             "피곤하고 기운이 없었다."
     ),
     QUESTION_7(
+            7,
             "내가 잘 못했거나, 실패했다는 생각이 들었다. / 혹은 자신과 가족을 실망시켰다고 생각했다."
     ),
     QUESTION_8(
+            8,
             "신문을 읽거나 TV를 보는 것과 같은 일상적인 일에도 집중할 수가 없었다."
     ),
     QUESTION_9(
+            9,
             "차라리 죽는 것이 더 낫겠다고 생각했다. / 혹은 자해할 생각을 했다."
     );
 
+    private final int index;
     private final String questionText;
 
-    PHQ9Question(String questionText) {
-        this.questionText = questionText;
-    }
-
-    public String getQuestionText() {
-        return questionText;
-    }
 }
--- src/main/java/com/konkuk/strhat/domain/selfDiagnosis/enums/PSSQuestion.java
+++ src/main/java/com/konkuk/strhat/domain/selfDiagnosis/enums/PSSQuestion.java
@@ -1,45 +1,54 @@
 package com.konkuk.strhat.domain.selfDiagnosis.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PSSQuestion {
 
     QUESTION_1(
+            1,
             "지난 한 달 동안, 예상치 못한 일이 생겨서 기분 나빠진 적이 얼마나 있었나요?"
     ),
     QUESTION_2(
+            2,
             "지난 한 달 동안, 중요한 일들을 통제할 수 없다고 느낀 적은 얼마나 있었나요?"
     ),
     QUESTION_3(
+            3,
             "지난 한 달 동안, 초조하거나 스트레스가 쌓인다고 느낀 적은 얼마나 있었나요?"
     ),
     QUESTION_4(
+            4,
             "지난 한 달 동안, 짜증나고 성가신 일들을 성공적으로 처리한 적이 얼마나 있었나요?"
     ),
     QUESTION_5(
+            5,
             "지난 한 달 동안, 생활 속에서 일어난 중요한 변화들을 효과적으로 대처한 적이 얼마나 있었나요?"
     ),
     QUESTION_6(
+            6,
             "지난 한 달 동안, 개인적인 문제를 처리하는 능력에 대해 자신감을 느낀 적은 얼마나 있었나요?"
     ),
     QUESTION_7(
+            7,
             "지난 한 달 동안, 자신의 뜻대로 일이 진행된다고 느낀 적은 얼마나 있었나요?"
     ),
     QUESTION_8(
+            8,
             "지난 한 달 동안, 매사를 잘 컨트롤하고 있다고 느낀 적이 얼마나 있었나요?"
     ),
     QUESTION_9(
+            9,
             "지난 한 달 동안, 당신이 통제할 수 없는 범위에서 발생한 일 때문에 화가 난 적이 얼마나 있었나요?"
     ),
     QUESTION_10(
+            10,
             "지난 한 달 동안, 어려운 일이 너무 많이 쌓여서 극복할 수 없다고 느낀 적이 얼마나 있었나요?"
     );
 
+    private final int index;
     private final String questionText;
 
-    PSSQuestion(String questionText) {
-        this.questionText = questionText;
-    }
-
-    public String getQuestionText() {
-        return questionText;
-    }
 }
