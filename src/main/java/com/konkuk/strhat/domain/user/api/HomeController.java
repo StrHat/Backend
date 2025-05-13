d8ba3b8f8c1444db37ef515a082dfb51d46b0042
You-Hyuk <cxz6411@naver.com>
2025-05-13 19:50:47 +0900

    fix: Post -> Get 변경

--- src/main/java/com/konkuk/strhat/domain/user/api/HomeController.java
+++ src/main/java/com/konkuk/strhat/domain/user/api/HomeController.java
@@ -5,6 +5,7 @@ import com.konkuk.strhat.domain.user.dto.GetHomeResponse;
 import com.konkuk.strhat.global.response.ApiResponse;
 import com.konkuk.strhat.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class HomeController {
 
     private final HomeService homeService;
 
-    @PostMapping("")
+    @GetMapping("")
     public ApiResponse<GetHomeResponse> getHomeInfo() {
         GetHomeResponse response = homeService.findHomeInfo(SecurityUtil.getCurrentUserId());
         return ApiResponse.success(response);
