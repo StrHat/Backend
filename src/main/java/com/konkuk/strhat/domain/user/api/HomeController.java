4c6b146adcef7d44a6b0baef787160cca830f586
You-Hyuk <cxz6411@naver.com>
2025-05-13 19:53:32 +0900

    remove: import 제거

--- src/main/java/com/konkuk/strhat/domain/user/api/HomeController.java
+++ src/main/java/com/konkuk/strhat/domain/user/api/HomeController.java
@@ -6,7 +6,6 @@ import com.konkuk.strhat.global.response.ApiResponse;
 import com.konkuk.strhat.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
