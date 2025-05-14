218ba3507728b0f799987a5ccfcf4f5ba92b0a96
You-Hyuk <cxz6411@naver.com>
2025-05-14 12:28:54 +0900

    fix: 자가진단 조회 api type query parameter 추가

--- src/main/java/com/konkuk/strhat/domain/selfDiagnosis/dao/SelfDiagnosisRepository.java
+++ src/main/java/com/konkuk/strhat/domain/selfDiagnosis/dao/SelfDiagnosisRepository.java
@@ -1,6 +1,7 @@
 package com.konkuk.strhat.domain.selfDiagnosis.dao;
 
 import com.konkuk.strhat.domain.selfDiagnosis.entity.SelfDiagnosis;
+import com.konkuk.strhat.domain.selfDiagnosis.enums.SelfDiagnosisType;
 import com.konkuk.strhat.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +10,5 @@ import java.util.Optional;
 
 public interface SelfDiagnosisRepository extends JpaRepository<SelfDiagnosis, Long> {
 
-    Optional<SelfDiagnosis> findBySelfDiagnosisDateAndUser(LocalDate date, User user);
+    Optional<SelfDiagnosis> findBySelfDiagnosisDateAndUserAndType(LocalDate date, User user, SelfDiagnosisType type);
 }
